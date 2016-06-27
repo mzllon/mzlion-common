a647cd9478708ff1039ba77c1a5ed094d0c37f9d
mzlion.home <and.mz.yq@gmail.com>
2016-06-27 08:22:18 +0800

    [easy-poi]增加BASE64

---
+++ mzlion-core/src/main/java/com/mzlion/core/lang/Base64.java
@@ -0,0 +1,35 @@
+package com.mzlion.core.lang;
+
+/**
+ * Created by mzlion on 2016/6/22.
+ */
+public class Base64 {
+
+    /**
+     * BASE64标准字符表
+     */
+    private static final byte[] STANDARD_ENCODE_TABLE = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+    };
+    /**
+     * URL安全的BASE64字符表，将标准字符表{@linkplain #STANDARD_ENCODE_TABLE}中的'+','-'替换为'-','_'
+     */
+    private static final byte[] URL_SAFE_ENCODE_TABLE = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'
+    };
+
+    public static void main(String[] args) {
+        String str = "Hello!";
+        byte[] data = str.getBytes();
+
+    }
+
+}
--- mzlion-core/src/test/java/com/mzlion/core/JustTest.java
+++ mzlion-core/src/test/java/com/mzlion/core/JustTest.java
@@ -1,7 +1,8 @@
 package com.mzlion.core;
 
-import com.mzlion.core.date.DateUtils;
 import org.junit.Test;
+
+import javax.xml.bind.DatatypeConverter;
 
 /**
  * Created by mzlion on 2016/5/22.
@@ -10,8 +11,10 @@ public class JustTest {
 
     @Test
     public void test() throws Exception {
-        System.out.println(DateUtils.formatDate(DateUtils.parseDateByLongStr("1464710400000")));
-        System.out.println(DateUtils.formatDate(DateUtils.parseDateByLongStr("1464753600000")));
+//        System.out.println(DateUtils.formatDate(DateUtils.parseDateByLongStr("1464710400000")));
+//        System.out.println(DateUtils.formatDate(DateUtils.parseDateByLongStr("1464753600000")));
+        System.out.println(DatatypeConverter.printBase64Binary("严".getBytes()));
+        System.out.println(DatatypeConverter.parseInteger("xsd:10"));
     }
 
 }
